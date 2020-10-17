305bfe37260050889438fbfa0a3f3927e53e6dc3
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2020-10-16 22:49:26 -0400

    Add Netlify Identiy plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Hello world!
+  <div data-netlify-identity-menu></div>
+  <div data-netlify-identity-button>Login with Netlify Identity</div>
+  </div>
 }
